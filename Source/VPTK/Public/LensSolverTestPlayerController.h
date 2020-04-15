d35f7c4dfcd6d35b6294a21239ab7a0e5308f47a
Sean Connor <sean@polypulse.io>
2020-04-15 15:04:10 -0600

    Implemented custom player controller.

---
+++ Source/VPTK/Public/LensSolverTestPlayerController.h
@@ -0,0 +1,21 @@
+#pragma once
+
+/*
+#include "CoreMinimal.h"
+#include "CoreTypes.h"
+
+#include "LensSolverTestPlayerController.generated.h"
+
+UCLASS(BlueprintType, Blueprintable)
+class ALensSolverTestPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+	public:
+
+	APlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : APlayerController(ObjectInitializer) 
+	{
+		bAutoManageActiveCameraTarget = false;
+	}
+};
+*/
