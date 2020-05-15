c1895828862e26783757e6eb755680366f4eeacd
Sean Connor <sean@polypulse.io>
2020-05-14 21:30:51 -0700

    Successful compile on 4.25

--- Source/LensCalibrator/LensCalibrator.Build.cs
+++ Source/LensCalibrator/LensCalibrator.Build.cs
@@ -14,7 +14,8 @@ public class LensCalibrator : ModuleRules
 		Console.WriteLine(isDebug ? "Compiling Debug() build." : "Compiling release build.");
 		
 		PublicIncludePaths.AddRange(
-			new string[] {
+			new string[] 
+			{
 				"Runtime/MediaAssets/Public",
 				"Runtime/ImageWriteQueue/Public"
 			}
@@ -22,16 +23,17 @@ public class LensCalibrator : ModuleRules
 				
 		
 		PrivateIncludePaths.AddRange(
-			new string[] {
-				Path.Combine(ModuleDirectory, "../../Source/ThirdParty/OpenCV/Include")
-			}
+                new string[] 
+				{
+                    Path.Combine(ModuleDirectory, "../../Source/ThirdParty/OpenCV/Include")
+                }
 			);
 			
 		
 		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-			}
+                new string[]
+                {
+                }
 			);
 			
 		
@@ -54,26 +56,15 @@ public class LensCalibrator : ModuleRules
 			);
 		
 		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-			}
-			);
-
 		string[] files = Directory.GetFiles(Path.Combine(ModuleDirectory, isDebug ? "../../Source/ThirdParty/OpenCV/Binaries/Debug/Static" : "../../Source/ThirdParty/OpenCV/Binaries/Release/Static"));
-		files = files.Where(f => Path.GetExtension(f) == ".lib").ToArray();
-		files = files.Where(s => !s.Contains("libpng.lib")).ToArray();
-		for (int i = 0; i < files.Length; i++)
-			Console.WriteLine(string.Format("Including lib: {0}", files[i]));
+
+		files = files.Where(f => {
+			return Path.GetExtension(f) == ".lib" && !f.Contains("libpng.lib");
+        }).ToArray();
+
 		PublicAdditionalLibraries.AddRange(files);
 
-		string libPNGPath = Path.Combine(EngineDirectory, "Source/ThirdParty/libPNG/libPNG-1.5.2/lib/Win64/VS2015/libpng_64.lib");
+		string libPNGPath = Path.Combine(EngineDirectory, "Source/ThirdParty/libPNG/libPNG-1.5.2/lib/Win64-llvm/Release/libpng15_static.lib");
 		PublicAdditionalLibraries.Add(libPNGPath);
-
-		/*
-		files = Directory.GetFiles(Path.Combine(ModuleDirectory, "../../ThirdParty/OpenCV/Binaries/Release/Dynamic"));
-		files = files.Where(f => Path.GetExtension(f) == ".dll").ToArray();
-		PublicDelayLoadDLLs.AddRange(files);
-		*/
 	}
 }
