e50473cd8a2d457dd6486992913bab061c29f466
Sean Connor <sean@polypulse.io>
2020-05-21 08:17:15 -0700

    Refactored OpenCV build process to dll.

--- Source/LensCalibrator/LensCalibrator.Build.cs
+++ Source/LensCalibrator/LensCalibrator.Build.cs
@@ -54,8 +54,37 @@ public class LensCalibrator : ModuleRules
                     "JsonUtilities"
                 }
 			);
-		
-		
+
+		string dllName = isDebug ? "opencv_world430d.dll" : "opencv_world430.dll";
+		string libName = isDebug ? "opencv_world430d.lib" : "opencv_world430.lib";
+
+		PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, isDebug ?
+			Path.Combine("../../Source/ThirdParty/OpenCV/Binaries/Debug/Static/", libName) :
+			Path.Combine("../../Source/ThirdParty/OpenCV/Binaries/Release/Static/", libName)));
+
+		PublicDelayLoadDLLs.Add(dllName);
+
+		string binaryDir = Path.Combine(ModuleDirectory, "../../Binaries/Win64/");
+		if (!Directory.Exists(binaryDir))
+			Directory.CreateDirectory(binaryDir);
+
+		string targetDLL = isDebug ?
+			Path.Combine("../../Source/ThirdParty/OpenCV/Binaries/Debug/Dynamic/", dllName) :
+			Path.Combine("../../Source/ThirdParty/OpenCV/Binaries/Release/Dynamic/", dllName);
+
+		string copyPath = Path.Combine(binaryDir, dllName);
+
+		if (!File.Exists(copyPath))
+		{
+			string from = Path.Combine(ModuleDirectory, targetDLL);
+			File.Copy(from, copyPath, true);
+
+			Console.WriteLine(string.Format("Copied from: \"{0}\" to: \"{1}\".", from, copyPath));
+		}
+
+		RuntimeDependencies.Add(Path.Combine(ModuleDirectory, targetDLL));
+
+		/*
 		string[] files = Directory.GetFiles(Path.Combine(ModuleDirectory, 
 			isDebug ? 
 			"../../Source/ThirdParty/OpenCV/Binaries/Debug/Static" : 
@@ -71,6 +100,7 @@ public class LensCalibrator : ModuleRules
 		Console.WriteLine("Linking against libPNG provided by Unreal Engine instead of the one provided by OpenCV.");
 		string libPNGPath = Path.Combine(EngineDirectory, "Source/ThirdParty/libPNG/libPNG-1.5.2/lib/Win64-llvm/Release/libpng15_static.lib");
 		PublicAdditionalLibraries.Add(libPNGPath);
+		*/
 		/*
 		foreach (string publicLib in PublicAdditionalLibraries)
 			Console.WriteLine(string.Format("Including additional public library: \"{0}\".", publicLib));
